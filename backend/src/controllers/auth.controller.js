b05b4cae2b9a7fbab3599ac261b02b72736fa3b0
Tejas Angadi <tejasangadi456@gmail.com>
2025-08-15 21:48:37 +0530

    Added the business logic for login and logout. Also fixed the route types for auth.

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -42,13 +42,13 @@ export const signup = async (req, res) => {
 
         const { password: _password, ...userWithoutPassword } = newUser.toObject();
         if (newUser) {
-             \generateToken(newUser._id, res);
+            generateToken(newUser._id, res);
             await newUser.save();
             res.status(201).json({
                 success: true,
                 message: "User created Successfully!",
                 data: userWithoutPassword
-            })
+            });
         }
         else {
             res.status(400).json({
@@ -66,10 +66,67 @@ export const signup = async (req, res) => {
     }
 }
 
-export const login = (req, res) => {
-    res.send("Login route");
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({
+            success: false,
+            message: "Fill all fields"
+        })
+    }
+
+    try {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid Credentials"
+            })
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password)
+        if (!isPasswordCorrect) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid Credentials"
+            })
+        }
+
+        const token = generateToken(user._id, res);
+        console.log("TOKEN: ", token)
+
+        const { password: _password, createdAt, updatedAt, ...userWithoutPassword } = user.toObject();
+        res.status(200).json({
+            success: true,
+            message: "User logged in successfully!",
+            data: userWithoutPassword
+        })
+    }
+    catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+            data: "Internal server while logging in."
+        })
+    }
 }
 
 export const logout = (req, res) => {
-    res.send("Logout route");
+    try {
+        res.clearCookie("token")
+
+        res.status(200).json({
+            success: true,
+            message: "Logged out successfully!"
+        })
+    }
+    catch (err) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+            data: "Internal server while logging out."
+        })
+    }
 }
